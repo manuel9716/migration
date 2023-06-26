43a91477405aed387939f957e9844fc654cbaafb
manuel9716 <manuelvalencia9716@gmail.com>
2023-06-26 07:27:40 +0200

    cambios_config_sqlmilton

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,35 +1,35 @@
 // CONEXION SQLSERVER PRUEBA LOCAL
-const config = {
-     user: 'sa',
-     password: '12345678',
-     server: 'localhost\\MSSQLSERVER01',
-     database: 'usuarios_bitwan',
-     options:{
-         trustedconnection: false,
-         enableArithAbort: true,
-         encrypt: false
-         //intancename: 'nombre instancia'
-     }
+// const config = {
+//      user: 'sa',
+//      password: '12345678',
+//      server: 'localhost\\MSSQLSERVER01',
+//      database: 'usuarios_bitwan',
+//      options:{
+//          trustedconnection: false,
+//          enableArithAbort: true,
+//          encrypt: false
+//          //intancename: 'nombre instancia'
+//      }
      
    
-}
+// }
 // CONEXION SQLSERVER
 
-// const config = {
-//     user: 'manuel2',
-//     password: 'ghbnLKJ/*+25',
-//     server: '10.0.0.12\\WORLDOFFICE019',
-//     //    server: '10.0.0.12',
-//     database: 'INSITEL S.A.S',
-//     options:{
-//         trustedconnection: false,
-//         enableArithAbort: true,
-//         encrypt: false
-//         //intancename: 'nombre instancia'
-//     }
+const config = {
+    user: 'manuel2',
+    password: 'ghbnLKJ/*+25',
+    server: '10.0.0.12\\WORLDOFFICE019',
+    //    server: '10.0.0.12',
+    database: 'INSITEL S.A.S',
+    options:{
+        trustedconnection: false,
+        enableArithAbort: true,
+        encrypt: false
+        //intancename: 'nombre instancia'
+    }
     
   
-// }
+}
 
 
 
